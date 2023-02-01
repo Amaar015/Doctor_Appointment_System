63f2f2979fc389f5ff65f0c72f4d64029bd24759
Amaar Hassnain <93473921+Amaar015@users.noreply.github.com>
2023-02-01 11:53:55 +0500

    updated

---
+++ models/userModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+
+})
+
+const userModel = mongoose.model('userdb', UserSchema);
+module.exports = userModel;
