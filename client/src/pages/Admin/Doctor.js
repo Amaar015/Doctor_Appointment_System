a33f3087c35819c4293b2fc46fab839544a609e8
Amaar Hassnain <93473921+Amaar015@users.noreply.github.com>
2023-02-21 14:32:17 +0500

    added a docter and user page with data

--- client/src/pages/Admin/Doctor.js
+++ client/src/pages/Admin/Doctor.js
@@ -1,11 +1,63 @@
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../../componets/Layout'
-
+import axios from 'axios'
+import { genComponentStyleHook } from 'antd/es/theme/internal';
+import { Table } from 'antd';
 const Doctor = () => {
+    const [doctors, setDoctror] = useState([]);
+    const getDoctor = async () => {
+        try {
+            const res = await axios.get('/api/v1/admin/getAllDocter', {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+            if (res.data.success) {
+                setDoctror(res.data.data);
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    useEffect(() => {
+        getDoctor()
+    }, [])
+
+    const columns = [
+        {
+            title: "Name",
+            dataIndex: "firstName",
+
+        },
+        {
+            title: "Email",
+            dataIndex: 'email',
+        },
+        {
+            title: "doctor",
+            dataIndex: "isDoctor",
+            render: (text, record) => (
+                <span>{record.isDoctor ? "yes" : "no"}</span>
+            )
+        },
+        {
+            title: "Actions",
+            dataIndex: "actions",
+            render: (text, recode) => (
+                <div className="d-flex">
+                    <button className="btn btn-danger">Block</button>
+                </div>
+            )
+        }
+
+    ]
+
     return (
+
         <Layout>
-            <h1>All doctors are here</h1>
+            <h1 className='text-center m-2'>All doctors are here</h1>
+            <Table columns={columns} dataSource={doctors} />
         </Layout>
     )
 }
--- client/src/pages/Admin/User.js
+++ client/src/pages/Admin/User.js
@@ -1,10 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../../componets/Layout'
-
+import axios from 'axios'
+import { Table } from 'antd'
 const User = () => {
+    const [users, setusers] = useState([])
+
+    //   get all user
+    const getUsers = async () => {
+        try {
+            const res = await axios.get('/api/v1/admin/getAllUser', {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+            if (res.data.success) {
+                setusers(res.data.data)
+            }
+        } catch (error) {
+            console.log(error)
+
+        }
+    }
+
+    useEffect(() => {
+        getUsers()
+    }, [])
+    //   AntD tbale col
+
+    const columns = [
+        {
+            title: "Name",
+            dataIndex: "name"
+        },
+        {
+            title: "Email",
+            dataIndex: "email"
+        },
+        {
+            title: "Doctor",
+            dataIndex: "isDoctor",
+            render: (text, record) => (
+                <span>{record.isDoctor ? "yes" : "no"}</span>
+            )
+        },
+        {
+            title: "Actions",
+            dataIndex: "actions",
+            render: (text, recode) => (
+                <div className="d-flex">
+                    <button className="btn btn-danger">Block</button>
+                </div>
+            )
+        }
+    ]
+
     return (
         <Layout>
-            <h1>Show all Users</h1>
+            <h1 className='text-center m-2'>Show all Users</h1>
+            <Table columns={columns} dataSource={users} />
         </Layout>
     )
 }
